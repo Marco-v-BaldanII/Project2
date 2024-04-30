f5360ad9b50f5c82b082df7a7105a4c26e7e7792
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2024-04-30 10:15:43 +0200

    .

--- citm_desvj_project_template-L16_Profiling_Solution/Game/Source/Render.cpp
+++ citm_desvj_project_template-L16_Profiling_Solution/Game/Source/Render.cpp
@@ -404,26 +404,26 @@ bool Render::DrawTextButton(const char* text, int posx, int posy, int w, int h, 
 void Render::DrawVignette(int centerX, int centerY, int radius, Uint8 maxAlpha)
 {
 	// Calculate the alpha value based on distance from the center
-	for (int y = centerY - radius; y <= centerY + radius; ++y)
-	{
-		for (int x = centerX - radius; x <= centerX + radius; ++x)
-		{
-			// Calculate distance from center (pythagoras)
-			int distanceSq = (x - centerX) * (x - centerX) + (y - centerY) * (y - centerY);
+	//for (int y = centerY - radius; y <= centerY + radius; ++y)
+	//{
+	//	for (int x = centerX - radius; x <= centerX + radius; ++x)
+	//	{
+	//		// Calculate distance from center (pythagoras)
+	//		int distanceSq = (x - centerX) * (x - centerX) + (y - centerY) * (y - centerY);
 
-			// Calculate alpha based on distance (the bigger the distance the more alpha)
-			Uint8 alpha = (Uint8)(maxAlpha * (0 + ((double)(distanceSq) / (radius * radius))));
+	//		// Calculate alpha based on distance (the bigger the distance the more alpha)
+	//		Uint8 alpha = (Uint8)(maxAlpha * (0 + ((double)(distanceSq) / (radius * radius))));
 
-			// Clamp alpha to ensure it's within valid range
-			alpha = Clamp(alpha, 0, maxAlpha);
+	//		// Clamp alpha to ensure it's within valid range
+	//		alpha = Clamp(alpha, 0, maxAlpha);
 
-			// Set render color with the calculated alpha
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, alpha);
+	//		// Set render color with the calculated alpha
+	//		SDL_SetRenderDrawColor(renderer, 0, 0, 0, alpha);
 
-			// Render a point at (x, y)
-			SDL_RenderDrawPoint(renderer, x, y);
-		}
-	}
+	//		// Render a point at (x, y)
+	//		SDL_RenderDrawPoint(renderer, x, y);
+	//	}
+	//}
 }
 
 // L14: TODO 6: Implement a method to load the state
